e005dac7fa13c3021ca20bffd99d968b2439d70a
Mirai Kuriyama <62803790+Mirai3103@users.noreply.github.com>
2021-06-08 09:53:08 +0700

    fix copy

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function myFunction(id) {
-    var element = document.getElementById(id);
-    element.select()
-    document.execCommand("copy");
-    alert("Copied text" );
-  }
-  function copyToClipboard(id) {
-    var from= document.getElementById(id);
-    // var fromz = fromz.getElementsByClassName('card-body')
-    var range = document.createRange();
-    window.getSelection().removeAllRanges();
-    range.selectNode(from);
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+  var element = document.getElementById(id);
+  element.select();
+  document.execCommand("copy");
+  alert("Copied text");
 }
-function changedrop(){
-  var element = document.activeElement.parentElement
-  if(element.classList.contains("dropup")){
-    element.classList.remove("dropup")
-  }else{
+function copyToClipboard(id) {
+  var from = document.getElementById(id).firstElementChild;
+  var range = document.createRange();
+  window.getSelection().removeAllRanges();
+  range.selectNode(from);
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+}
+function changedrop() {
+  var element = document.activeElement.parentElement;
+  if (element.classList.contains("dropup")) {
+    element.classList.remove("dropup");
+  } else {
     element.classList.add("dropup");
   }
 }
