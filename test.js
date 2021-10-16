1f9cc92f058e213609b9ab203737921b2e662916
Mirai Kuriyama <Huuhoag1412@gmail.com>
2021-10-16 19:37:56 +0700

    first commit

--- test.js
+++
@@ -1,7 +0,0 @@
-
-let a = setInterval(function(){ let allbutton = document.getElementsByClassName("rq0escxv l9j0dhe7 du4w35lb j83agx80 g5gj957u rj1gh0hx buofh1pr hpfvmrgz taijpn5t bp9cbjyn owycx6da btwxx1t3 d1544ag0 tw6a2znq jb3vyjys dlv3wnog rl04r1d5 mysgfdmx hddg9phg qu8okrzs g0qnabr5")
-allbutton  =[...allbutton].filter((button) => button.innerText == "Thích" && button.concat != 'dalike')
-for(let i =0; i < allbutton.length; i++){
-    allbutton[i].click();
-    allbutton[i].concat = 'dalike';
-}}, 10000);
